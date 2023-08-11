8c47861edd18ce7b48ac5a90c54acdec339e13a6
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-11 00:01:09 -0300

    update
    Adicionando botão menu superior

--- src/components/layout/MenuAbove/index.js
+++ src/components/layout/MenuAbove/index.js
@@ -2,7 +2,11 @@ import './MenuAbove.css'
 const MenuAbove =()=>{
   return(
     <div className="container-menuAbove">
-  
+      <p>Descanso</p>
+      <p>Estoque</p>
+      <p>Agenda</p>
+      <p>Descanso</p>
+      <p>Anotação</p>
     </div>
   )
 }
