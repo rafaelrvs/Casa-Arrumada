d7713fd81e3579476525519c6e5255934e1e7c39
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-12 11:16:32 -0300

    Stock in progress
    build page of stock

--- src/components/Itens/Estoque/index.js
+++ src/components/Itens/Estoque/index.js
@@ -3,8 +3,17 @@ import './Estoque.css'
 
 const Estoque = () =>{
   return(
-    <div className="Estoque-container">
-  
+    <div className="estoque-container">
+        <form action="">
+          <input type="text" placeholder='Nome do produto' />
+          <textarea name="description" id="description" cols="30" rows="10"></textarea>
+          <div>
+
+          <input type="submit" value="Adicionar" />
+            <p>Remover</p>
+          </div>
+       
+        </form>
     </div>
   )
 }
--- src/components/layout/MenuAbove/index.js
+++ src/components/layout/MenuAbove/index.js
@@ -1,22 +1,41 @@
 import { useState } from 'react'
 import './MenuAbove.css'
 import Relogio from '../../Itens/Relogio'
+import Estoque from '../../Itens/Estoque'
+import SelectField from '../SelectedField'
 const MenuAbove =()=>{
   const [activeWacthPresentation,setActiveWacthPresentation] = useState(false)
-function activeWacth(event){
+  const [activeStockPresentation,setActiveStockPresentation] = useState(false)
+  const [activeFieldBackground,setActiveFieldBackground] = useState(false)
+
+
+  function activeWacth(event){
   setActiveWacthPresentation(true)
-event.preventDefault()
+  setActiveStockPresentation(false)
+  setActiveFieldBackground(false)
+  event.preventDefault()
 }
+function activeStock(event){
+  setActiveStockPresentation(true)
+  setActiveWacthPresentation(false)
+  setActiveFieldBackground(true)
+  event.preventDefault()
+}
+
+
+
+
   return(
     <div className="container-menuAbove">
       <section>
-
       <p onClick={activeWacth}>Descanso</p>
-      <p>Estoque</p>
+      <p onClick={activeStock}>Estoque</p>
       <p>Agenda</p>
       <p>Avisos</p>
       <p>Anotação</p>
       {activeWacthPresentation&&<Relogio/>}
+      {activeStockPresentation&&<Estoque/>}
+      {activeFieldBackground&&<SelectField/>}
       </section>
     </div>
   )
