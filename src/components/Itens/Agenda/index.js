3cac48612c649667d4ca88789d57f8fc7d751990
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-15 09:37:34 -0300

    update
    Adicionando lista de meses

--- src/components/Itens/Agenda/index.js
+++ src/components/Itens/Agenda/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+
 import './Agenda.css';
+class listMonth31{
+  constructor(mes){
+    this._meses = [
+      'janeiro',
+      'março',
+      'maio',
+      'julho',
+      'agosto',
+      'outubro',
+      'dezembro',
+    ]
+    this.monthSelct = mes
+
+  }
+  get _meses(){
+    return this._meses;
+  }
+  get monthSelct(){
+    return this.monthSelct;
+  }
+
+}
 
 class Agenda extends React.Component {
   state = {
+    
     daysInMonth: 31, // Altere para o número de dias no mês atual
   };
 
@@ -30,7 +54,25 @@ class Agenda extends React.Component {
   render() {
     return (
       <div className='container__Agenda'>
+        <div className='container__btn_option'>
+
         <h2>Calendário</h2>
+        <select name="" id="selectMonth">
+          <option value="'janeiro">Janeiro  </option>
+          <option value="'fevereiro">Fevereiro  </option>
+          <option value="'março">Março  </option>
+          <option value="'maio">Abril  </option>
+          <option value="'julho">Maio  </option>
+          <option value="'agosto">Junho  </option>
+          <option value="'outubro">Julho  </option>
+          <option value="'dezembro">Agosto  </option>
+          <option value="'dezembro">Setembro  </option>
+          <option value="'dezembro">Outubro  </option>
+          <option value="'dezembro">Novembro  </option>
+          <option value="'dezembro">Dezembro  </option>
+        </select>
+
+        </div>
         <table className="calendar">
           <thead>
             <tr>
