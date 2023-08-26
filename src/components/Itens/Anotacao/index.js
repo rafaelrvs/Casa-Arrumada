ce5bb821be558547f3a6cd845dde5e496ea9cab5
rafaelrvs <rafaelvieira1333@gmail.com>
2023-08-26 17:34:47 -0300

    criando campo de anotação

--- src/components/Itens/Anotacao/index.js
+++ src/components/Itens/Anotacao/index.js
@@ -1,9 +1,40 @@
 import './Anotacao.css'
-function Anotacao() {
-return(
-  <div>
+import React, { useState } from 'react';
 
-  </div>
+
+function Anotacao() {
+  const [notes, setNotes] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddNote = () => {
+    if (inputValue.trim() !== '') {
+      setNotes([...notes, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <div className="blocoDeNotas">
+      <h1>Meu Bloco de Notas</h1>
+      <br />
+      <div className="note-container">
+        <input
+          type="text"
+          placeholder="Digite sua nota..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handleAddNote}>Adicionar Nota</button>
+      </div>
+      <div className="notes-list">
+        <h2>Notas:</h2>
+        <ul>
+          {notes.map((note, index) => (
+            <li key={index}>{note}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
