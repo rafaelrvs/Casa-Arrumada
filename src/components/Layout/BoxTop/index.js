cf81ab67018c5d02034b2b2c319bfc5a38e52cd1
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-16 11:47:55 -0300

    atualização responsividade anotação e adicionando funções

--- src/components/Layout/BoxTop/index.js
+++ src/components/Layout/BoxTop/index.js
@@ -6,25 +6,43 @@ import { useState } from "react";
 import Cronometro from "../../Cronometro";
 import TextContentInSideBox from "../TextContentInSideBox";
 import "./BoxTop.css";
+import Anotacao from "../../Anotacao";
 
 const BoxTop = () => {
   const [cronometro, setCronometro] = useState(false);
+  const [anotation, setAnotation] = useState(false);
 
   const activePage = (event) => {
     setCronometro(true);
+    setAnotation(false);
     event.preventDefault();
   };
+  const handlerOpenNotation = (event) => {
+    setCronometro(false);
+    setAnotation(true);
+    event.preventDefault();
+  };
+
   return (
     <div className="container__BoxTop">
       <div className="content">
         <div onClick={activePage}>
           <TextContentInSideBox /> Cronometro
         </div>
-        <TextContentInSideBox /> Anotação
-        <TextContentInSideBox /> Exercicio laboral
-        <TextContentInSideBox /> importante
+        <div onClick={handlerOpenNotation}>
+          <TextContentInSideBox /> Anotação
+        </div>
+        <div>
+          <TextContentInSideBox /> importante
+        </div>
+        <div>
+          <TextContentInSideBox /> Exercicio laboral
+        </div>
       </div>
-      <div>{cronometro && <Cronometro />}</div>
+      <div>
+        {cronometro && <Cronometro />}
+        {anotation && <Anotacao />}
+      </div>
     </div>
   );
 };
