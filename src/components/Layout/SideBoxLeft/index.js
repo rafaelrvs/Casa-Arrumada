3a1f382503b4883d108a48addd6d4a8677ef26d1
rafaelrvs <rafaelvieira1333@gmail.com>
2023-09-17 22:15:47 -0300

    feat atualizando funcionalidade do decimo terceiro

--- src/components/Layout/SideBoxLeft/index.js
+++ src/components/Layout/SideBoxLeft/index.js
@@ -1,26 +1,49 @@
 /*
  * Container that represents the side box
-*/
+ */
 
+import { useState } from "react";
 import Logo from "../Logo";
 import TextContentInSideBox from "../TextContentInSideBox";
 import "./SideBoxLeft.css";
+import DescimoTerceiro from "../../DecimoTerceiro";
+
+const SideBoxLeft = () => {
+const [decimoAtivo,setDecimoAtivo] = useState(false);
 
 
-const SideBoxLeft =()=>{
-
-  return(
+  function activeModelCalcDecimo(event){
+    setDecimoAtivo(true);
+    event.preventDefault();
+  }
+  return (
     <div className="container__box__left">
-      <div><img className="maisImg" src="./img/mais.svg" alt="" /></div>
       <div>
-        <Logo/>
+        <img className="maisImg" src="./img/mais.svg" alt="" />
       </div>
-     <TextContentInSideBox/>Calculo de ferias
-     <TextContentInSideBox/>Calculo de decimo terceiro
-     <TextContentInSideBox/>Calculo de Hora Extra
-     <TextContentInSideBox/>Calculo de Adicional noturno
-     
+      <div>
+        <Logo />
+      </div>
+      <div>
+        <TextContentInSideBox />
+        Calculo de ferias
+      </div>
+      <div onClick={activeModelCalcDecimo}>
+        <TextContentInSideBox />
+        Calculo de decimo terceiro
+      </div>
+      <div>
+        <TextContentInSideBox />
+        Calculo de Hora Extra
+      </div>
+      <div>
+        <TextContentInSideBox />
+        Calculo de Adicional noturno
+      </div>
+
+
+      {decimoAtivo&&<DescimoTerceiro/>}
     </div>
   );
-}
+};
 export default SideBoxLeft;
